1e9b7569ba2741e204d781a4b0e44a12cf6b8d97
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-26 23:54:08 +0330

    start page style added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
-    <main>
-      <div></div>
-      <div>
-        <button>Get Started</button>
+    <main className="min-h-screen flex flex-col justify-end bg-cover bg-center bg-bg-main" style={{backgroundImage: 'url("your-background-image.jpg")'}}>
+      <div className="flex justify-center pb-10">
+        <button className="bg-white hover:bg-gray-100 transition-colors py-3 px-24 rounded-lg shadow-lg focus:outline-none focus:shadow-outline">Get Started</button>
       </div>
     </main>
   );
