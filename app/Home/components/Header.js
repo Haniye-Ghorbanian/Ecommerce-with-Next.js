a8131a4a89608356eb64dc634e810407ff302c69
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-27 21:53:59 +0330

    filters style fixed

--- app/Home/components/Header.js
+++ app/Home/components/Header.js
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <div className="flex flex-row-reverse items-center justify-between py-4 px-6 bg-white">
-      <div className="rounded-full bg-purple-700 w-10 h-10 overflow-hidden"></div>
+      <div className="rounded-full bg-main w-10 h-10 overflow-hidden"></div>
       
       <img className="h-7 w-7" src="menu.png"></img>
      
--- app/Home/components/filters.js
+++ app/Home/components/filters.js
@@ -1,7 +1,15 @@
 export default function Filters() {
-    return (
-        <div className="mb-4">
-          
-        </div>
-      );
+  return (
+    <div className="mb-4 w-full flex items-center  h-16 px-6 overflow-scroll">
+      <div>
+        <input type="checkbox" className="hidden" id="checkbox" />
+        <label
+          htmlFor="checkbox"
+          className="flex items-center justify-center h-9 w-24 px-3 py-2 bg-white border border-solid border-gray-200 text-center mr-3 rounded-md cursor-pointer transition-colors duration-200 group"
+        >
+          electron
+        </label>
+      </div>
+    </div>
+  );
 }
