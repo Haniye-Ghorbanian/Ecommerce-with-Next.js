49a56c384587fb70c5599c461a7a1e2d6cb22153
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-28 02:57:38 +0330

    footer design added

--- app/Home/components/footerMenu.js
+++ app/Home/components/footerMenu.js
@@ -1,25 +1,26 @@
 export default function FooterMenu() {
     return (
-      <div className="fixed bottom-0 left-0 w-full bg-white shadow-md py-4 px-4 flex justify-between items-center">
-        <ul className="flex space-x-4">
+      <div className="fixed bottom-0 left-0 w-full h-20 bg-white  py-4 px-4 flex justify-between items-center">
+        <ul className="flex items-center justify-between space-x-4 w-full h-full px-6">
           <li>
             <a href="#" className="text-gray-600 hover:text-gray-800">
-              Home
+              <div className="h-6 w-6"><img src="home.png"></img></div>
+              
             </a>
           </li>
           <li>
             <a href="#" className="text-gray-600 hover:text-gray-800">
-              Shop
+            <div className="h-6 w-6"><img src="shopping-cart.png"></img></div>
             </a>
           </li>
           <li>
             <a href="#" className="text-gray-600 hover:text-gray-800">
-              About Us
+            <div className="h-6 w-6"><img src="wishlist.png"></img></div>
             </a>
           </li>
           <li>
             <a href="#" className="text-gray-600 hover:text-gray-800">
-              Contact
+            <div className="h-6 w-6"><img src="favourite.png"></img></div>
             </a>
           </li>
         </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
     extend: {
       colors: {
         "main": "#400D54"
+      },
+      boxShadow: {
+        "myShadow": "box-shadow: -3px -1px 20px 7px rgb(64 13 84 / 28%)"
       }
     },
   },
