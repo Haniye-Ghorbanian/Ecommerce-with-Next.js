bf2e390bf3a3a9c542c68b2ba0f8999ff86c59ab
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-28 04:16:40 +0330

    products got displayed

--- app/Home/components/productItem.js
+++ app/Home/components/productItem.js
@@ -1,12 +1,14 @@
-export default function ProductItem() {
+export default function ProductItem(props) {
+  console.log(props)
+const {title, image, category, price } = props.product;
   return (
-    <div className="w-full flex items-center justify-start border border-solid border-gray-200 h-40 rounded-md p-3">
-      <div className=" bg-main w-2/5 aspect-square"></div>
+    <div className="w-full flex items-center justify-start border border-solid border-gray-200 h-40 rounded-md p-2">
+      <img className=" w-2/5 aspect-square object-contain p-5" src={image}></img>
 
-      <div className="h-full flex flex-col items-start justify-between py-5 px-5 ">
-        <h1 className="font-bold text-lg">adaad</h1>
-        <p className="text-sm font-light">dfadf afddfadf dasda</p>
-        <span className="text-lg font-bold">79</span>
+      <div className="h-full flex flex-col items-start justify-between py-1 px-5 ">
+        <h1 className="font-bold text-lg overflow-x-scroll">{title}</h1>
+        <p className="text-sm font-light">{category}</p>
+        <span className="text-md font-bold">{`$${price}`}</span>
       </div>
     </div>
   );
--- app/Home/components/products.js
+++ app/Home/components/products.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
 import ProductItem from "./productItem";
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://fakestoreapi.com/products"
+      );
+      const ProductsData = await response.json();
+      console.log(ProductsData);
+      setProducts(ProductsData);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="w-full h-96 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-scroll px-6 fixed top-96">
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
-      <ProductItem />
+    <div className="w-full h-96 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 overflow-scroll px-6 pb-4 fixed top-96">
+      {products.map(product => (<ProductItem key={product.id} product={product}/>))}
     </div>
   );
 }
