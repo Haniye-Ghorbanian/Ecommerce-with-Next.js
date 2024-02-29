717c690286fde6ae5716940e899e264cd1ce74e2
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-02-29 10:06:02 +0330

    add vercel link to readme

--- README.md
+++ README.md
@@ -2,6 +2,9 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
+Visit the website on: 
+(https://ecommerce-with-next-js-bay.vercel.app/)
+
 First, run the development server:
 
 ```bash
--- app/Home/components/product/productItem.js
+++ app/Home/components/product/productItem.js
@@ -1,12 +1,11 @@
 export default function ProductItem(props) {
-  // console.log(props)
 const {title, image, category, price } = props.product;
   return (
     <div className="w-full flex items-center justify-start border border-solid border-gray-200 h-40 rounded-md p-2">
       <img className=" w-2/5 aspect-square object-contain p-5" src={image}></img>
 
-      <div className="h-full flex flex-col items-start justify-between py-1 px-5 ">
-        <h1 className="font-bold text-md overflow-x-scroll">{title}</h1>
+      <div className="h-full flex flex-col items-start justify-between py-1 px-5">
+        <h1 className="font-bold text-md overflow-x-scroll mb-2">{title}</h1>
         <p className="text-sm font-light">{category}</p>
         <span className="text-md font-bold">{`$${price}`}</span>
       </div>
