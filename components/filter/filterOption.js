e23d265afcb53edc310f718b8e38fce46ae22b2b
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2024-03-07 11:11:35 +0330

    filter input checked state style fixed

--- components/filter/filterOption.js
+++ components/filter/filterOption.js
@@ -13,16 +13,18 @@ export default function FilterOption(props) {
   };
 
   return (
-    <div>
-      <input
-        type="checkbox"
-        className="hidden"
-        id={props.category}
-        onChange={handleFilter}
-      />
+    <div className="">
+      
+        <input
+          type="checkbox"
+          className="hidden peer"
+          id={props.category}
+          onChange={handleFilter}
+        />
+      
       <label
         htmlFor={props.category}
-        className="flex items-center justify-center h-9 w-44 px-4 py-3 bg-white border border-solid border-gray-200 text-center mr-3 rounded-md cursor-pointer transition-colors duration-200 group"
+        className="flex items-center justify-center h-9 w-44 px-4 py-3 bg-white border border-solid border-gray-200 text-center mr-3 rounded-md cursor-pointer transition-colors duration-200 peer-checked:bg-main peer-checked:text-white"
       >
         {props.category}
       </label>
